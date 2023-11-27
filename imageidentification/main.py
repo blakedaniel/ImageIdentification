e90127209953b0afbc9a7fd75d381d7cfdd3fdca
blakedaniel <blake.vanfleteren@gmail.com>
2023-11-27 09:37:10 -0500

    updated main.py

--- imageidentification/main.py
+++ imageidentification/main.py
@@ -1,99 +1,115 @@
-from duckduckgo_search import DDGS
+import os
 import torch
 import torchvision
-import torchvision.transforms as transforms
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
+from torchvision import datasets, models, transforms
+from torch.utils.data import DataLoader
+from PIL import Image
 
-with DDGS() as ddgs:
-    keywords = 'cat'
-    ddgs_images_gen = ddgs.images(
-      keywords,
-      region="wt-wt",
-      safesearch="off",
-      timelimit='y',
-      size=None,
-      color="Monochrome",
-      type_image=None,
-      layout=None,
-      license_image=None,
-      max_results=10,
-    )
-    for r in ddgs_images_gen:
-        print(r)
+class ImageClassifactionTuner:
+    def __init__(self, dir:str='./classification_data') -> None:
+        # Specify the data directory
+        self.data_dir = dir
+        # Define the device to use (M1 GPU or CPU)
+        self.device = torch.device("mps" if torch.cuda.is_available() else "cpu")
 
-breakpoint()
+    def define_image_transformation(self, crop:int=224, resize:int=256):
+        # Define the data transformations for training and validation
+        self.data_transforms = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(crop),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(resize),
+                transforms.CenterCrop(crop),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+            ]),
+        }
+    def load_data(self):
+        self.image_datasets = {x: datasets.ImageFolder(os.path.join(self.data_dir, x), self.data_transforms[x]) for x in ['train', 'val']}
+        self.dataloaders = {x: DataLoader(self.image_datasets[x], batch_size=4, shuffle=True) for x in ['train', 'val']}
+        self.dataset_sizes = {x: len(self.image_datasets[x]) for x in ['train', 'val']}
+        self.class_names = self.image_datasets['train'].classes
+        
+    def load_model(self, model=models.resnet18(weights=models.ResNet18_Weights.DEFAULT), output_size:int=2,):
+        # Load a pre-trained model (e.g. ResNet-18) and modify the last layer
+        num_ftrs = model.fc.in_features
+        model.fc = nn.Linear(num_ftrs, output_size)
+        self.model = model.to(self.device)
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-# Load and normalize CIFAR10
-class ImageClassification:
-    def __init__(self, root:str='./data', classes:tuple=('plane', 'car', 'bird', 'cat',)):
-        self.transform = transforms.Compose(
-            [transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
-        self.trainset = torchvision.datasets.CIFAR10(root=root, train=True,
-                                                download=True, transform=self.transform)
-        self.trainloader = torch.utils.data.DataLoader(self.trainset, batch_size=4,
-                                                shuffle=True, num_workers=2)
+    def train_model(self, num_epochs:int=10):
+        # Train the model
+        for epoch in range(num_epochs):
+            print(f'Epoch {epoch}/{num_epochs - 1}')
+            print('-' * 10)
 
-        self.testset = torchvision.datasets.CIFAR10(root=root, train=False,
-                                            download=True, transform=self.transform)
-        self.testloader = torch.utils.data.DataLoader(self.testset, batch_size=4,
-                                                shuffle=False, num_workers=2)
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    self.model.train()
+                else:
+                    self.model.eval()
 
-        self.classes = classes
+                running_loss = 0.0
+                running_corrects = 0
 
-# Define a Convolutional Neural Network
+                for inputs, labels in self.dataloaders[phase]:
+                    inputs = inputs.to(self.device)
+                    labels = labels.to(self.device)
+                    self.optimizer.zero_grad()
 
-class Net(nn.Module):
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = self.model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = self.criterion(outputs, labels)
+                        if phase == 'train':
+                            loss.backward()
+                            self.optimizer.step()
+                            
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
 
-    def forward(self, x):
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return x
+                epoch_loss = running_loss / self.dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / self.dataset_sizes[phase]
 
-image_classification = ImageClassification()
-net = Net()
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
 
-# Define a loss function and optimizer
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+            print()
+        
+    def save_model(self):
+        torch.save(self.model.state_dict(), 'model.pth')
+        
+    def predict(self, image_path:str):
+        # Load the model
+        self.model.load_state_dict(torch.load('model.pth'))
+        self.model.eval()
+        # Load the image
+        image = Image.open(image_path)
+        # Apply the transformations
+        image_tensor = self.data_transforms['val'](image).float()
+        image_tensor = image_tensor.unsqueeze_(0)
+        # Move to the GPU
+        input = image_tensor.to(self.device)
+        # Predict the class of the image
+        output = self.model(input)
+        index = output.data.cpu().numpy().argmax()
+        return self.class_names[index]
 
-# Train the network
-for epoch in range(2):  # loop over the dataset multiple times
-
-    running_loss = 0.0
-    for i, data in enumerate(image_classification.trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        inputs, labels = data
-
-        # zero the parameter gradients
-        optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
-
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
-            running_loss = 0.0
-
-print('Finished Training')
+if __name__ == '__main__':
+    ict = ImageClassifactionTuner()
+    ict.define_image_transformation()
+    ict.load_data()
+    ict.load_model()
+    ict.train_model()
+    ict.save_model()
+    print(ict.predict('./data/test/ALSTONIA.jpg'))
+    print(ict.predict('./data/test/Bay-Leaves-Vs.-Basil.jpeg'))
+    
+    
